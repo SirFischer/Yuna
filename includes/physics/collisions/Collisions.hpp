770b9a1554aef71b8238a888379f666dd95c96ac
Marek <marek5886@hotmail.com>
2021-11-06 13:03:40 +0100

    added new collision functions

--- includes/physics/collisions/Collisions.hpp
+++ includes/physics/collisions/Collisions.hpp
@@ -4,7 +4,7 @@
  * File Created: Saturday, 30th October 2021 9:57:34 am
  * Author: Marek Fischer
  * -----
- * Last Modified: Saturday, 6th November 2021 8:11:27 am
+ * Last Modified: Saturday, 6th November 2021 11:37:57 am
  * Modified By: Marek Fischer 
  * -----
  * Copyright - 2021 Deep Vertic
@@ -12,6 +12,7 @@
 #pragma once
 
 #include <SFML/Graphics.hpp>
+#include <math.h>
 
 namespace Yuna
 {
@@ -20,6 +21,8 @@ namespace Yuna
 		bool	AABBCollision(const sf::FloatRect &pRect1, const sf::FloatRect &pRect2);
 		bool	LineLineCollision(const sf::Vector2f &pLine1Start, const sf::Vector2f &pLine1End, const sf::Vector2f &pLine2Start, const sf::Vector2f &pLine2End);
 		bool	LineRectCollision(const sf::Vector2f &pLineStart, const sf::Vector2f &pLineEnd, const sf::FloatRect &pRect);
+		bool	RayRectCollision(const sf::Vector2f &pRayStart, const sf::Vector2f &pRayDir, const sf::FloatRect &pRect, sf::Vector2f &pContactPoint, sf::Vector2f &pContactNormal, float &pNearTime);
+		bool	DynamicRectCollision(const sf::FloatRect &pRect1, const sf::Vector2f &pVelocity, const sf::FloatRect &pRect2, sf::Vector2f &pContactPoint, sf::Vector2f &pContactNormal, float &pNearTime);
 	} // namespace Physics
 } // namespace Yuna
 
--- src/physics/collisions/Collisions.cpp
+++ src/physics/collisions/Collisions.cpp
@@ -4,7 +4,7 @@
  * File Created: Saturday, 30th October 2021 9:58:27 am
  * Author: Marek Fischer
  * -----
- * Last Modified: Saturday, 6th November 2021 8:13:25 am
+ * Last Modified: Saturday, 6th November 2021 12:51:58 pm
  * Modified By: Marek Fischer 
  * -----
  * Copyright - 2021 Deep Vertic
@@ -41,6 +41,63 @@ namespace Yuna
 			bool	bottom = LineLineCollision(pLineStart, pLineEnd, sf::Vector2f(pRect.left, pRect.top + pRect.height), sf::Vector2f(pRect.left + pRect.width, pRect.top + pRect.height));
 
 			return (left || right || top || bottom);
+		}
+
+		bool	RayRectCollision(const sf::Vector2f &pRayStart, const sf::Vector2f &pRayDir, const sf::FloatRect &pRect, sf::Vector2f &pContactPoint, sf::Vector2f &pContactNormal, float &pNearTime)
+		{
+			sf::Vector2f	near = sf::Vector2f((pRect.left - pRayStart.x) / pRayDir.x, (pRect.top - pRayStart.y) / pRayDir.y);
+			sf::Vector2f	far = sf::Vector2f((pRect.left + pRect.width - pRayStart.x) / pRayDir.x, (pRect.top + pRect.height - pRayStart.y) / pRayDir.y);
+
+			if (std::isnan(far.y) || std::isnan(far.x))
+				return (false);
+			if (std::isnan(near.y) || std::isnan(near.x))
+				return (false);
+
+			if (near.x > far.x)
+				std::swap(near.x, far.x);
+			if (near.y > far.y)
+				std::swap(near.y, far.y);
+
+			if (near.x > far.y || near.y > far.x)
+				return (false);
+
+			pNearTime = std::max(near.x, near.y);
+			float pFarTime = std::min(far.x, far.y);
+
+			if (pFarTime < 0)
+				return (false);
+
+			pContactPoint = sf::Vector2f(pRayStart.x + (pNearTime * pRayDir.x), pRayStart.y + (pNearTime * pRayDir.y));
+			if (near.x > near.y)
+			{
+				if ((1.f / pRayDir.x) < 0)
+					pContactNormal = sf::Vector2f(1.f, 0.f);
+				else
+					pContactNormal = sf::Vector2f(-1.f, 0.f);
+			}
+			else if (near.x < near.y)
+			{
+				if ((1.f / pRayDir.y) < 0)
+					pContactNormal = sf::Vector2f(0.f, 1.f);
+				else
+					pContactNormal = sf::Vector2f(0.f, -1.f);
+			}
+			return (true);
+		}
+
+		bool	DynamicRectCollision(const sf::FloatRect &pRect1, const sf::Vector2f &pVelocity, const sf::FloatRect &pRect2, sf::Vector2f &pContactPoint, sf::Vector2f &pContactNormal, float &pNearTime)
+		{
+			if (pVelocity.x == 0.f && pVelocity.y == 0.f)
+				return (false);
+			sf::FloatRect expanded;
+			expanded.left = pRect2.left - (pRect1.width / 2.f);
+			expanded.top = pRect2.top - (pRect1.height / 2.f);
+			expanded.width = pRect2.width + pRect1.width;
+			expanded.height = pRect2.height + pRect1.height;
+
+			if (RayRectCollision(sf::Vector2f(pRect1.left + pRect1.width / 2.f, pRect1.top + pRect1.height / 2.f), pVelocity, expanded, pContactPoint, pContactNormal, pNearTime))
+				return (pNearTime >= 0.f && pNearTime < 1.f);
+			return (false);
 		}
 	} // namespace Physics
 } // namespace Yuna
