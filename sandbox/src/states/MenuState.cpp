05a4431a59efe7b1c44cd7de941bfb42cf5a6f4a
Marek <marek5886@hotmail.com>
2022-04-20 05:26:54 +0200

    fixed sandbox

--- sandbox/src/states/MenuState.cpp
+++ sandbox/src/states/MenuState.cpp
@@ -7,7 +7,7 @@ MenuState::MenuState(Yuna::Core::Window* tWindow)
 	mStateAction = Yuna::Core::eStateControls::EXIT;
 	mf::GUI::ClearWidgets();
 	bool *active = &mActive;
-	mf::GUI::AddWidget(mf::Button::Create(sf::Color::Green, sf::Color::Red)->SetClickEvent([active](){
+	mf::GUI::AddWidget(mf::Button::Create()->SetClickEvent([active](){
 		*active = false;
 	})->SetPositionPercentage(true, false)->SetPosition(sf::Vector2f(45, 200)));
 }
